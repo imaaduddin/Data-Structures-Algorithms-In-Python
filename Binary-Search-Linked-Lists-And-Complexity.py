2cfc38bcf13234aea34b3d8bf438898a51cc4da3
Imaad Uddin <imaaduddin@icloud.com>
2022-01-19 16:27:49 -0600

    follow the steps with the function

--- Binary-Search-Linked-Lists-And-Complexity.py
+++ Binary-Search-Linked-Lists-And-Complexity.py
@@ -11,3 +11,32 @@
 # 6. Apply the right technique to overcome the inefficiency. Repeat steps 3 to 6. 
 
 
+# Test Cases
+cards = [13, 11, 10, 7, 4, 3, 1, 0]
+query = 7
+output = 3
+
+# Locate card function
+def locate_card(cards, query):
+    # Create a variable position with the value 0
+    position = 0
+    
+    # Set up a loop for repetition 
+    while True:
+        # Check if element at the current position matches the query 
+        if cards[position] == query:
+            # Answer found! Return and exit...
+            return position 
+        # Increment the position 
+        position += 1
+        
+        # Check if we have reached the end of the array
+        if position == len(cards):
+            # Number not found, return -1
+            return -1
+
+# Testing the function 
+result = locate_card(cards, query)
+print(result)
+
+
