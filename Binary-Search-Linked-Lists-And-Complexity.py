55af64d99f2a7699b83a31d90d07a5d626bb8cbc
Imaad Uddin <imaaduddin@icloud.com>
2022-01-19 15:59:24 -0600

    6 steps to solve problems

--- Binary-Search-Linked-Lists-And-Complexity.py
+++ Binary-Search-Linked-Lists-And-Complexity.py
@@ -1,1 +1,13 @@
-# Question 1: Alice has some cards with numbers written on them
+# Question 1: Alice has some cards with numbers written on them. She arranges the cards in decreasing order, and lays
+# them out face down in sequence on a table. She challenges Bob to pick out the card containing a given number by turning 
+# over as few cards as possible. Write a function to help Bob locate the card.
+
+# Systematic Strategy To Apply For Solving Problems:
+# 1. State the problem clearly. Identify the input & output formats.
+# 2. Come up with some example inputs & outputs. Try to cover all edge cases.
+# 3. Come up with a correct solution for the problem. State it in plain English.
+# 4. Implement the solution and test it using example inputs. Fix bugs, if any.
+# 5. Analyze the algorithm's complexity and identity inefficiencies, if any.
+# 6. Apply the right technique to overcome the inefficiency. Repeat steps 3 to 6. 
+
+
