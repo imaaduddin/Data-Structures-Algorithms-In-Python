3902e77b289a131e7026cd60063d0517102e74ea
Imaad Uddin <imaaduddin@icloud.com>
2022-01-26 17:31:15 -0600

    linear vs Binary search

---
+++ Binary-vs-Linear-Search.py
@@ -0,0 +1,8 @@
+# Linear
+def locate_card(cards, query):
+    position = 0
+    while position < len(cards):
+        if cards[position] == query:
+            return position 
+        position += 1
+    return -1
